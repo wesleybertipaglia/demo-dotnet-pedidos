1c45e7d7bb24a5520e725bcfef4b1090a6528a11
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-27 12:49:20 -0300

    feat: add PedidosController
    added to Pedidos.API.Controllers namespace

---
+++ Pedidos.API/Controllers/PedidosController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Pedidos.Application.DTOs;
+using Pedidos.Application.Interfaces;
+
+namespace Pedidos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+        private readonly IPedidoService _pedidoService;
+
+        public PedidosController(IPedidoService pedidoService)
+        {
+            _pedidoService = pedidoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PedidoReadDTO>>> GetAllPedidos()
+        {
+            var pedidos = await _pedidoService.GetAllPedidos();
+            return Ok(pedidos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PedidoReadDTO>> GetPedidoById(Guid id)
+        {
+            var pedido = await _pedidoService.GetPedidoById(id);
+            return Ok(pedido);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PedidoReadDTO>> CreatePedido()
+        {
+            var pedido = await _pedidoService.CreatePedido();
+            return CreatedAtAction(nameof(GetPedidoById), new { id = pedido.Id }, pedido);
+        }
+
+        [HttpPost("{id}/close")]
+        public async Task<ActionResult<PedidoReadDTO>> ClosePedido(Guid id)
+        {
+            var pedido = await _pedidoService.ClosePedido(id);
+            return Ok(pedido);
+        }
+
+        [HttpPost("{id}/items")]
+        public async Task<ActionResult<PedidoReadDTO>> AddItemPedido(Guid id, Guid produtoId, int quantidade)
+        {
+            var pedido = await _pedidoService.AddItemPedido(id, produtoId, quantidade);
+            return Ok(pedido);
+        }
+
+        [HttpDelete("{id}/items/{produtoId}")]
+        public async Task<ActionResult<PedidoReadDTO>> RemoveItemPedido(Guid id, Guid produtoId)
+        {
+            var pedido = await _pedidoService.RemoveItemPedido(id, produtoId);
+            return Ok(pedido);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePedido(Guid id)
+        {
+            await _pedidoService.DeletePedido(id);
+            return NoContent();
+        }
+    }
+}
