583c9db7a1a13905f4cb71e8da75bb888b2618b9
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-27 13:01:13 -0300

    feat: add Startup.cs
    Added to Pedidos.API

---
+++ Pedidos.API/Startup.cs
@@ -0,0 +1,50 @@
+using Pedidos.Application.Interfaces;
+using Pedidos.Application.Services;
+using Pedidos.Domain.Interfaces;
+using Pedidos.Infrastructure.Extensions;
+using Pedidos.Infrastructure.Repositories;
+using AutoMapper;
+
+namespace Pedidos.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInMemoryDatabase();
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddScoped<IProdutoService, ProdutoService>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IPedidoService, PedidoService>();
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pedidos.API v1"));
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
