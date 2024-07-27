366a4f01a1c0c81e9ccc61addbd5154fa2c00370
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-27 12:37:57 -0300

    feat: add PedidoService
    added to Pedidos.Application.Services namespace

---
+++ Pedidos.Application/Services/PedidoService.cs
@@ -0,0 +1,60 @@
+using Pedidos.Application.DTOs;
+using Pedidos.Application.Interfaces;
+using Pedidos.Domain.Interfaces;
+using AutoMapper;
+
+namespace Pedidos.Application.Services
+{
+    public class PedidoService : IPedidoService
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly IMapper _mapper;
+
+        public PedidoService(IPedidoRepository pedidoRepository, IMapper mapper)
+        {
+            _pedidoRepository = pedidoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<PedidoReadDTO>> GetAllPedidos()
+        {
+            var pedidos = await _pedidoRepository.GetAllPedidos();
+            return _mapper.Map<IEnumerable<PedidoReadDTO>>(pedidos);
+        }
+
+        public async Task<PedidoReadDTO> GetPedidoById(Guid id)
+        {
+            var pedido = await _pedidoRepository.GetPedidoById(id);
+            return _mapper.Map<PedidoReadDTO>(pedido);
+        }
+
+        public async Task<PedidoReadDTO> CreatePedido()
+        {
+            var pedido = await _pedidoRepository.CreatePedido();
+            return _mapper.Map<PedidoReadDTO>(pedido);
+        }
+
+        public async Task<PedidoReadDTO> ClosePedido(Guid id)
+        {
+            var pedido = await _pedidoRepository.ClosePedido(id);
+            return _mapper.Map<PedidoReadDTO>(pedido);
+        }
+
+        public async Task<PedidoReadDTO> AddItemPedido(Guid id, Guid produtoId, int quantidade)
+        {
+            var pedido = await _pedidoRepository.AddItemPedido(id, produtoId, quantidade);
+            return _mapper.Map<PedidoReadDTO>(pedido);
+        }
+
+        public async Task<PedidoReadDTO> RemoveItemPedido(Guid id, Guid produtoId)
+        {
+            var pedido = await _pedidoRepository.RemoveItemPedido(id, produtoId);
+            return _mapper.Map<PedidoReadDTO>(pedido);
+        }
+
+        public async Task DeletePedido(Guid id)
+        {
+            await _pedidoRepository.DeletePedido(id);
+        }
+    }
+}
