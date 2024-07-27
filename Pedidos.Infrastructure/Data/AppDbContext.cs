5caf31f8fbf715ca99c6c895e7d0755be6483dfd
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-27 11:43:44 -0300

    feat: add AppDbContext
    added to Pedidos.Infrastructure.Data namespace

---
+++ Pedidos.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Pedidos.Domain.Entities;
+
+namespace Pedidos.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
